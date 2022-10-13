4b024a3af8c5104e9076d0f0de2a8303206bea5a
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-10-13 18:21:21 +0300

    ...

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 4;
+        int result;
+        for (int i = 1; i < 10; i++) {
+            result = i * k;
+            System.out.println(i + " * " + k + " = " + result);
+        }
+    }
+}
