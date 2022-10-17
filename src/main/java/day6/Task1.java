eb35a15cc1a7fab230da5ea49367444496f1f343
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-10-17 19:41:19 +0300

    Marathon 2020 day 6

--- src/main/java/day6/Task1.java
+++ src/main/java/day6/Task1.java
@@ -2,6 +2,12 @@ package day6;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.info();
 
+        Motorbike motorbike = new Motorbike(2000, "dark", "vvv");
+        motorbike.info();
+        int dif = motorbike.yearDifference(2022);
+        System.out.println("Разница в годах " + dif);
     }
 }
