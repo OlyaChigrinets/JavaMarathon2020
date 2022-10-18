e3b2a88e6fce9baae8c3f791a29d06e1bfa01f35
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-10-18 16:02:24 +0300

    Marathon 2020 day 7

---
+++ src/main/java/day7/Airplane.java
@@ -0,0 +1,47 @@
+package day7;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private  int fuel;
+
+    public Airplane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + this.producer + ", "  + "год выпуска: " + this.year + ", " +
+                            "длина: " + this.length + ", " + "вес: " + this.weight + ", " +
+                            "количество топлива в баке: " + this.fuel + ".");
+    }
+    public void fillUp(int fuelAdd) {
+        this.fuel += fuelAdd;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public static void compareAirplanes(Airplane airplane, Airplane airplane1) {
+        if (airplane.getLength() > airplane1.getLength())
+            System.out.println();
+    }
+}
