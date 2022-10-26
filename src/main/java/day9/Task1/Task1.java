5290b36c648443d9a04318ae8ae267a5c312da2a
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-10-26 16:57:13 +0300

    Marathon 2020 day 9

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -3,5 +3,9 @@ package day9.Task1;
 public class Task1 {
     public static void main(String[] args) {
 
+        Student student = new Student("Оля", 6514);
+        Teacher teacher = new Teacher("Ирина Васильевна", "Английский");
+        student.printInfo();
+        teacher.printInfo();
     }
 }
