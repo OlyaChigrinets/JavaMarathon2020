aeed622adb96bd7025b1b0be32e2a5c6e9715824
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-10-26 16:56:22 +0300

    Marathon 2020 day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,10 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (int i = 0; i < 2001; i++) {
+            stringBuilder.append(i).append(" ");
+        }
+        System.out.println(stringBuilder);
     }
 }
