2d1b6a1e3c4374c77164a2bc38f8496d52608af4
OlyaChigrinets <helga.chigrinets@gmail.com>
2022-11-02 18:05:29 +0300

    Marathon 2020 day 12

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,22 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<String> string = new ArrayList<>();
+        string.add("BMW");
+        string.add("AUDI");
+        string.add("MERC");
+        string.add("HUNDAY");
+        string.add("HONDA");
 
+        System.out.println(string);
+
+        string.add(3, "opel");
+        string.remove(0);
+
+        System.out.println(string);
     }
 }
